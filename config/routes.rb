18ea7ac6c1ebdaea798f5c18aed5ec058f871989
yuna-kusuhara <narlocked.222.b@gmail.com>
2020-01-13 12:44:45 +0900

    remove_group-c&create_request-c

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root "groups#index"
-  get 'groups/index'
+  root "requests#index"
+  get 'requests/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
