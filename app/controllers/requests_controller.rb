d8840c708b01cbab821c3ceaf65d806addccf2a9
yuna-kusuhara <narlocked.222.b@gmail.com>
2020-01-23 01:14:11 +0900

    create new-request&view_complete

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,11 +1,15 @@
 class RequestsController < ApplicationController
   def index
+    @requests = Request.all
   end
 
   def new
+    @request = Request.new
   end
 
   def create
+    Request.create(request_params)
+    redirect_to root_path
   end
 
   def edit
@@ -13,5 +17,10 @@ class RequestsController < ApplicationController
 
   def update
   end
-  
+
+  private
+
+  def request_params
+    params.require(:request).permit(:title, :item, :price, :location, :datelimit, :image, :map_irl).merge(create_user: current_user.id)
+  end
 end
