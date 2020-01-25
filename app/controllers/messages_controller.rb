826c25428158692ed4cde49428da2674544d265e
yuna-kusuhara <narlocked.222.b@gmail.com>
2020-01-25 16:50:35 +0900

    create_message-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,24 @@
 class MessagesController < ApplicationController
+  before_action :set_request
 
   def index
     @message = Message.new
-    @request = Request.find(params[:request_id])
     @messages = @request.messages.includes(:user)
   end
 
   def create
+    @message = @request.messages.new(message_params)
+    @message.save
+    redirect_to request_messages_path(@request)
   end
 
+  private
+
+  def set_request
+    @request = Request.find(params[:request_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
+  end
 end
