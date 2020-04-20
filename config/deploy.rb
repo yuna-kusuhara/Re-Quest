864e8e58162368efd9a7da4a0558457cb12812ed
yuna-kusuhara <narlocked.222.b@gmail.com>
2020-04-20 16:00:09 +0900

    fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,6 @@ set :repo_url,  'git@github.com:yuna-kusuhara/Re-Quest.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, %w{.env}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
@@ -26,17 +25,9 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-before 'deploy:starting', 'deploy:upload'
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-    # 上記linked_filesで使用するファイルをアップロードするタスク
-    desc 'Upload .env'
-    task :upload do
-      on roles(:app) do |host|
-        upload!('.env', "#{shared_path}/config/.env")
-      end
-    end
   task :restart do
     invoke 'unicorn:restart'
   end
